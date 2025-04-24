171c8b2e6e2582f28739070703dcbb2979ac532d
luan048 <luanmonteiropedrosa@gmail.com>
2025-04-24 16:56:30 -0300

    feat: elementos do navbar foram adicionados (logo da furia e icons dos canais da furia)

--- front-end/MainPage/MainPage.jsx
+++ front-end/MainPage/MainPage.jsx
@@ -1,11 +1,23 @@
 import React from "react";
-
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import './MainPage.css'
+import '../Icons/icons.js'
+
 function MainPage() {
     return (
         <>
         <div className="general-div">
-            <div className="nav-bar"></div>
+            <div className="nav-bar">
+                <img src="./imgs/Logo-Furia-navbar.svg" className="logoFuria-navbar" />
+                <FontAwesomeIcon className="icon-twitter" icon="fa-brands fa-twitter" />
+                <FontAwesomeIcon icon="fa-brands fa-instagram" />
+                <FontAwesomeIcon icon="fa-brands fa-facebook" />
+                <FontAwesomeIcon icon="fa-brands fa-youtube" />
+                <FontAwesomeIcon icon="fa-brands fa-twitch" />
+                <FontAwesomeIcon icon="fa-brands fa-tiktok" />
+                <FontAwesomeIcon icon="fa-brands fa-discord" />
+            </div>
+
         </div>
 
         <div className="footer"></div>
