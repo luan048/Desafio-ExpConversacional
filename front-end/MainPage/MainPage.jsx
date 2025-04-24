80f03f436b844b51bd1295974bc40cb218487dfc
luan048 <luanmonteiropedrosa@gmail.com>
2025-04-23 23:45:25 -0300

    feat: estrutura com hierarquia de arquivos e estrutura inicial do main page adicionados

---
+++ front-end/MainPage/MainPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './MainPage.css'
+function MainPage() {
+    return (
+        <>
+        <div className="general-div">
+            <div className="nav-bar"></div>
+        </div>
+
+        <div className="footer"></div>
+        </>
+    )
+}
+
+export default MainPage
