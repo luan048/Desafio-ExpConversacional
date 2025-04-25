5265d622c9d3a878fc4c99ac1fb4be494cbd0b96
luan048 <luanmonteiropedrosa@gmail.com>
2025-04-24 22:51:59 -0300

    feat: elementos do lado esquerdo da general-div adicionados

--- front-end/MainPage/MainPage.jsx
+++ front-end/MainPage/MainPage.jsx
@@ -6,7 +6,23 @@ import '../Icons/icons.js'
 function MainPage() {
     return (
         <> 
-            <div className="general-div"></div>
+            <div className="general-div">
+                <div className="content-div">
+
+                    <div className="elements-left">
+                        <div className="div-title" style={{'display': 'flex'}}>
+                            <img src="../imgs/Furia_Esports_logo.png" className="logo-furia" /> 
+                            <h1>Furia</h1>
+                        </div>
+
+                        <h1>Bem-vindo ao FURIA CS Chat Bot!</h1>
+                        <p style={{'fontFamily': 'Oswald'}}>Converse com nosso bot para receber as últimas notícias, partidas e muito mais</p>
+                        <button>Start Chat</button>
+                    </div>
+
+                    <div className="elements-right"></div>
+                </div>
+            </div>
 
             <div className="footer">
                 <div className="footer-esquer">
