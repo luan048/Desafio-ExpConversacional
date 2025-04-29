90d454d6d8d12a48e0ef3889435684b24dfcb7ae
luan048 <luanmonteiropedrosa@gmail.com>
2025-04-28 23:35:23 -0300

    feat: adicionando as demais funções do chatbot (função voltar; encaminhamento loja; história)

--- front-end/MainPage/MainPage.jsx
+++ front-end/MainPage/MainPage.jsx
@@ -3,7 +3,6 @@ import { useState, useEffect, useRef } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './MainPage.css'
 import '../Icons/icons.js'
-import { text } from "@fortawesome/fontawesome-svg-core";
 
 function MainPage() {
     // Variaveis de desbloqueio e inicio do chat
@@ -16,6 +15,9 @@ function MainPage() {
     const [userInput, setUserInput] = useState('')
     const [userName, setUserName] = useState('')
     const [userEmail, setUserEmail] = useState('')
+
+
+    const respostaEscolhasUser = userInput.trim().toLowerCase()
 
     // Função de desbloqueio e inicio chat
     const startChat = () => {
@@ -39,13 +41,13 @@ function MainPage() {
 
         setMessages([
             ...newMessages,
-            { sender: 'bot', text: `Valeu, ${userName}! Acabei de guardar aqui.` },
+            { sender: 'bot', text: `Valeu, ${userName}!` },
             {sender: 'bot', text: 'Agora vamos continuar nossa conversa. Me diz ai, o que você gostaria de saber sobre a furia?'},
             {text: 'Calendário de jogos'},
             {text: 'Resultados Recentes'},
             {text: 'Loja Furiosa'},
-            {text: 'Noticias'},
-            {text: 'Curiosidades'}
+            {text: 'História da Furia'},
+            {text: 'História time de CS da Furia'}
         ])
         setStep(3)
     }
@@ -60,6 +62,9 @@ function MainPage() {
                     ...prevMessages,
                     { sender: 'bot', text: 'Aqui está o calendário de jogos:' },
                     ...data.map(row => ({text: `${row[2]} dia ${row[0]} às ${row[1]}` })),
+                    {sender: 'bot', text: 'Saiba mais sobre os jogos (Digite sua opção)'},
+                    {text: 'Somente jogos de hoje'},
+                    {text: 'Voltar'}
                 ])
             }
             else {
@@ -75,8 +80,203 @@ function MainPage() {
         }
     }
 
+    const filtrarJogosHoje = async () => {
+        try {
+            const response = await fetch('http://localhost:3000/api/get-calendar')
+            const data = await response.json()
+    
+            const hoje = new Date()
+            const diaHoje = hoje.getDate()
+            const mesHoje = hoje.getMonth() + 1
+            const anoHoje = hoje.getFullYear()
+    
+            const jogosHoje = data.filter(row => {
+                const [dia, mes, ano] = row[0].split('/').map(Number)
+                return dia === diaHoje && mes === mesHoje && (ano ? ano === anoHoje : true)
+            })
+    
+            if (jogosHoje.length > 0) {
+                setMessages(prevMessages => [
+                    ...prevMessages,
+                    {sender: 'bot', text: 'Aqui estão os jogos de hoje:'},
+                    ...jogosHoje.map(row => ({ text: `${row[2]} às ${row[1]}`})),
+                    {text: '(Digite voltar para iniciar um novo chat)'}
+                ])
+            } 
+            else {
+                setMessages(prevMessages => [
+                    ...prevMessages,
+                    {sender: 'bot', text: 'Não encontramos jogos para hoje.' },
+                    {text: '(Digite voltar para iniciar um novo chat)'}
+                ])
+            }
+        } 
+        catch (error) {
+            console.error('Erro ao buscar jogos de hoje:', error)
+        }
+    }
+    
+
+    const fetchLastResults = async() => {
+        try{
+            const response = await fetch('http://localhost:3000/api/get-results')
+            const data = await response.json()
+
+            if(data && data.length) {
+                setMessages(prevMessages => [
+                    ...prevMessages,
+                    {sender: 'bot', text: 'Aqui está o resultado dos último jogos:'},
+                    ...data.map(row => ({text: `${row[1]} do dia ${row[0]}`})),
+                    {sender: 'bot', text: 'Fique por dentro dos próximos jogos (Digite sua opção)'},
+                    {text: 'Próximos jogos'},
+                    {text: 'Voltar'}
+                ])
+            }
+            else {
+                setMessages(prevMessages => [
+                    ...prevMessages,
+                    { sender: 'bot', text: 'Desculpe, não encontrei o calendário de jogos.' },
+                ])
+            }
+        }
+        catch(error) {
+            console.log('Erro ao tentar buscar os último resulados: ', error);
+        }
+    }
+    
+    const encaminhamentoLoja = (input) => {
+        const newMessages = [...messages, {sender: 'user', text: input}]
+        setMessages([
+            ...newMessages
+        ])
+        const resposta = input.trim().toLowerCase()
+        
+        if (resposta.includes("furia")) {
+            setMessages(prev => [
+                ...prev,
+                { sender: 'bot', text: 'Ok, vou te encaminhar para os produtos furia. Obrigado e um até logo!' }
+            ])
+        
+            setTimeout(() => {
+                window.location.href = 'https://www.furia.gg/produtos?filtro=Categoria__Furia'
+            }, 2000)
+        } 
+        else if ((resposta.includes("camisetas") || resposta.includes("camiseta")) && !resposta.includes("furia")) {
+            setMessages(prev => [
+                ...prev,
+                { sender: 'bot', text: 'Ok, vou te encaminhar para os produtos camisetas. Obrigado e um até logo!' }
+            ])
+        
+            setTimeout(() => {
+                window.location.href = 'https://www.furia.gg/produtos?filtro=Categoria__Camisetas'
+            }, 2000)
+        } 
+        else if ((resposta.includes("moletons") || resposta.includes("moletom")) && !resposta.includes("furia")) {
+            setMessages(prev => [
+                ...prev,
+                { sender: 'bot', text: 'Ok, vou te encaminhar para os produtos moletons. Obrigado e um até logo!' }
+            ])
+        
+            setTimeout(() => {
+                window.location.href = 'https://www.furia.gg/produtos?filtro=Categoria__Moletons'
+            }, 2000)
+        } 
+        else if ((resposta.includes("calças") || resposta.includes("calça")) && !resposta.includes("furia")) {
+            setMessages(prev => [
+                ...prev,
+                { sender: 'bot', text: 'Ok, vou te encaminhar para os produtos calças. Obrigado e um até logo!' }
+            ])
+        
+            setTimeout(() => {
+                window.location.href = 'https://www.furia.gg/produtos?filtro=Categoria__Calças'
+            }, 2000)
+        } 
+        else if ((resposta.includes("shorts") || resposta.includes("short")) && !resposta.includes("furia")) {
+            setMessages(prev => [
+                ...prev,
+                { sender: 'bot', text: 'Ok, vou te encaminhar para os produtos shorts. Obrigado e um até logo!' }
+            ])
+        
+            setTimeout(() => {
+                window.location.href = 'https://www.furia.gg/produtos?filtro=Categoria__Shorts'
+            }, 2000)
+        } 
+        else if ((resposta.includes("bonés") || resposta.includes("boné")) && !resposta.includes("furia")) {
+            setMessages(prev => [
+                ...prev,
+                { sender: 'bot', text: 'Ok, vou te encaminhar para os produtos bonés. Obrigado e um até logo!' }
+            ])
+        
+            setTimeout(() => {
+                window.location.href = 'https://www.furia.gg/produtos?filtro=Categoria__Bonés'
+            }, 2000)
+        } 
+        else {
+            setMessages(prev => [
+                ...prev,
+                { sender: 'bot', text: 'Ainda não tenho esse produto cadastrado, mas vou te mandar para o nosso site. Obrigado e um até logo!' }
+            ])
+        
+            setTimeout(() => {
+                window.location.href = 'https://www.furia.gg/'
+            }, 3000)
+        }
+        
+    }
+    
+    const filtroProdutoLoja = () => {
+        const newMessages = [...messages, {sender: 'user', text: userInput}]
+        setMessages([
+            ...newMessages,
+            {sender: 'bot', text: 'Certo, vou te encaminhar para nossa loja, mas antes disso, o que você procura? (Digite sua opção)'},
+            {text: 'Furia'},
+            {text: 'Camisetas'},
+            {text: 'Moletons'},
+            {text: 'Calças'},
+            {text: 'Shorts'},
+            {text: 'Bonés'},
+        ])
+    }
+
+    const contarHistoriaFuria = () => {
+        setMessages([
+            ...messages,
+            {sender: 'bot', text: 'A trajetória do time de CS da FURIA teve início em 2017, quando a organização foi criada em Uberlândia-MG pelos fundadores André Akkari, Jaime Pádua e Cris Guedes. Sob a liderança de Nicholas Nogueira, o Guerri, o primeiro elenco de Counter-Strike começou seus treinamentos e rapidamente passou a competir em torneios oficiais, demonstrando seu talento desde o princípio. Já em 2018, a FURIA recebeu o prêmio de Organização do Ano no Gamers Club Awards, e, em 2020, levantou o troféu da ESL Pro League Season 12, consolidando sua posição no cenário internacional. Desde então, a organização vem se destacando, conquistando novos títulos e expandindo sua presença em outras modalidades de esports! 🏆🔥'},
+            {text: '(Digite voltar para iniciar um novo chat)'}
+        ])
+        setStep(5)
+    }
+
+    const contarHistoriaTimeCS = () => {
+        setMessages([
+            ...messages,
+            {sender: 'bot', text: 'A trajetória da FURIA teve início em 2017, quando André Akkari, Jaime Pádua e Cris Guedes fundaram a organização em Uberlândia-MG. Seu primeiro time de Counter-Strike foi montado rapidamente e passou a competir, mudando-se para os Estados Unidos com o objetivo de conquistar espaço internacional. Desde então, a FURIA vem se destacando no cenário dos esports, acumulando conquistas e expandindo suas atividades para modalidades como League of Legends e VALORANT. Em 2020, a organização inaugurou um novo escritório em São Paulo e venceu a ESL Pro League. A FURIA segue crescendo, lançando linhas de roupas e participando de projetos sociais. A pantera, símbolo da organização, reflete toda a garra e determinação da equipe em se tornar uma referência mundial nos esports! 🐾🔥'},
+            {text: '(Digite voltar para iniciar um novo chat)'}
+        ])
+        setStep(6)
+    }
+
     const handleSendMessage = () => {
-        if (userInput.trim() === '') return
+        if (respostaEscolhasUser === '') return
+
+        if(respostaEscolhasUser === 'voltar') {
+            escolhaUser()
+            setStep(2)
+            setUserInput('')
+            return
+        }
+
+        if(respostaEscolhasUser === 'somente jogos de hoje') {
+            filtrarJogosHoje()
+            setUserInput('')
+            return
+        }
+
+        if(respostaEscolhasUser.includes("próximo" || respostaEscolhasUser.includes("próximos") && respostaEscolhasUser.includes("jogo") || respostaEscolhasUser.includes("jogos"))) {
+            fetchCalendarData()
+            setUserInput('')
+            return
+        }
 
         const newMessages = [...messages, { sender: 'user', text: userInput }]
 
@@ -99,11 +299,31 @@ function MainPage() {
 
             setMessages([
                 ...newMessages,
-            ])    
-            if(userInput.trim().toLowerCase().includes("calendário")) {
+            ])   
+
+            if(respostaEscolhasUser.includes("calendário")) {
                 fetchCalendarData()
             }
+            else if(respostaEscolhasUser.includes("resultado") || respostaEscolhasUser.includes("resultados")) {
+                fetchLastResults()
+            }
+            else if(respostaEscolhasUser.includes("loja") || respostaEscolhasUser.includes("loja furiosa")) {
+                filtroProdutoLoja()
+                setStep(4)
+                setUserInput('')
+                return
+            }
+            else if(respostaEscolhasUser.includes("história")) {
+                contarHistoriaFuria()
+            }
+            else if(respostaEscolhasUser.includes("história" && respostaEscolhasUser.includes("cs"))) {
+                contarHistoriaTimeCS()
+            }
             setStep(4)
+        }
+        else if(step === 4) {
+            encaminhamentoLoja(userInput)
+            setStep(5)
         }
 
         setUserInput('')
